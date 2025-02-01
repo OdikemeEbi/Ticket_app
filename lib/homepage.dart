60cfd7832d3079d0b16d746e053d2f843b47339f
OdikemeEbi <alaborabiri@gmail.com>
2025-02-01 16:34:26 +0100

    This is my first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int counter = 0;
+
+  increamentCount() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  decreamentCount() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text(
+          "Asdin Flutter Demo",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: const Icon(Icons.menu),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Hello World',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              counter.toString(),
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 40),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FloatingActionButton(
+              backgroundColor: Colors.green,
+              onPressed: () {
+                decreamentCount();
+                print(counter);
+              },
+              child: const Icon(Icons.remove),
+            ),
+            FloatingActionButton(
+              backgroundColor: Colors.green,
+              onPressed: () {
+                increamentCount();
+                print(counter);
+              },
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
